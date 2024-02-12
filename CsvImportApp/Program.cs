e42f3ab27197e8f2092e47683b02252d9cd3fe70
linddev87 <linddev87@gmail.com>
2024-02-12 09:41:36 +0100

    adding console app

---
+++ CsvImportApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Application.Models;
+using Application.Services;
+using Domain.Entities;
+
+namespace CsvImportApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = CsvFileHandler<MovieImportDto>.LoadFromPath(args[0]);
+        }
+    }
+}
